d4f77d1de4e8278a0caa9369efe0a8824e3f7986
leeonmichael <leeonmichael0@gmail.com>
2026-05-26 10:49:13 -0500

    Commit inicial: Base del servidor Don Alberto

---
+++ app.py
@@ -0,0 +1,8 @@
+# --- CÓDIGO BASE DE LA APLICACIÓN ---
+print("Servidor GigaMoto / Don Alberto Iniciado")
+
+# FASE 1: Programando la ruta compleja del inventario (A MEDIAS)
+# TODO: Terminar la lógica de persistencia del inventario
+def api_inventario():
+    print("Buscando repuestos en el inventario...")
+    # Código incompleto...
