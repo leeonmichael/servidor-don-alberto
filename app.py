a2a1f93c406a903d3af815e4c3f2bde1bf1630a0
leeonmichael <leeonmichael0@gmail.com>
2026-05-26 18:24:13 -0500

    Merge: Integracion de fix y feautre inventario

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # --- CÓDIGO DEL SERVIDOR DON ALBERTO ---
 print("Servidor GigaMoto Iniciado")
 
-<<<<<<< HEAD
 # FASE 1: Programando la ruta compleja del inventario (A MEDIAS)
 # TODO: Terminar la lógica de persistencia del inventario
 def api_inventario():
@@ -9,11 +8,9 @@ def api_inventario():
     # AGREGANDO LOGICA NUEVA DE INVENTARIO
 def consultar_stock():
     print("Buscando repuestos en la base de datos...")
-=======
 # FASE 3: Reparación de emergencia (Hotfix)
 # Fix: Forzar formateo de placas a mayúsculas en producción
 def api_peritajes(placa):
     placa_formateada = placa.upper()
     print(f"Placa procesada de forma segura: {placa_formateada}")
     return placa_formateada
->>>>>>> hotfix-placas-mayusculas
