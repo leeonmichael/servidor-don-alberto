b7059da98e83aa7c39a9ccfef0d1888495262fa1
leeonmichael <leeonmichael0@gmail.com>
2026-06-02 15:27:58 -0500

    Design: Enlazando ruta principal al modulo de inventario

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ print("Servidor GigaMoto / Don Alberto Iniciado")
 def api_inventario():
     print("Buscando repuestos en el inventario...")
     # Sabotaje intencional para la actividad del 2 de junio
+
     causar_error = 1 / 0
+    
     # Código incompleto...
