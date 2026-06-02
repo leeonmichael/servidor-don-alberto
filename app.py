3bc9079476f083b304431f0a80eab7b21987456f
leeonmichael <leeonmichael0@gmail.com>
2026-06-02 10:48:24 -0500

    Design: Estructurando modulo de inventario avanzado

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # --- CÓDIGO BASE DE LA APLICACIÓN ---
 print("Servidor GigaMoto / Don Alberto Iniciado")
-
+   
 # FASE 1: Programando la ruta compleja del inventario (A MEDIAS)
 # TODO: Terminar la lógica de persistencia del inventario
 def api_inventario():
     print("Buscando repuestos en el inventario...")
+    # Sabotaje intencional para la actividad del 2 de junio
+    causar_error = 1 / 0
     # Código incompleto...
